1eef8a0d3c47a33051ae4b13129915520ddc53f0
IrvanSaragi24 <127461456+IrvanSaragi24@users.noreply.github.com>
2023-05-26 15:41:44 +0700

    Initial Commit

---
+++ Git/GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitApp.swift
+//  Git
+//
+//  Created by Irvan P. Saragi on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
